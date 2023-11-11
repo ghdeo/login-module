41e6888007fdbf30e7b6f7d7d3af66cb773f464f
ghdeo <goldencorn7@naver.com>
2023-11-12 00:31:25 +0900

    test: 기업 회원 생성자 테스트 추가

---
+++ src/test/java/ghdeo/login/entity/CompanyUserTest.java
@@ -0,0 +1,35 @@
+package ghdeo.login.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyUserTest {
+    @Test
+    public void 기업_회원_생성자_테스트() throws Exception {
+        // given
+        // when
+        CompanyUser companyUser = new CompanyUser(
+                1L,
+                "admin@example.com",
+                "password",
+                "Test Company",
+                "123-456-7890",
+                "ABC123",
+                true,
+                LocalDateTime.now()
+        );
+
+        // then
+        assertNotNull(companyUser.getCorp_seq());
+        assertEquals("admin@example.com", companyUser.getEmail());
+        assertEquals("password", companyUser.getCorp_pw());
+        assertEquals("Test Company", companyUser.getCorp_name());
+        assertEquals("123-456-7890", companyUser.getPhone());
+        assertEquals("ABC123", companyUser.getCrop_code());
+        assertEquals(true, companyUser.isCorp_auth());
+        assertNotNull(companyUser.getCreated_at());
+    }
+}

f747786a1e9387921c11665593aa6e9c45db49bf
ghdeo <goldencorn7@naver.com>
2023-11-13 11:07:54 +0900

    feat: CompanyUserRepository에 findByEmail 추가

--- src/main/java/ghdeo/login/repository/CompanyUserRepository.java
+++ src/main/java/ghdeo/login/repository/CompanyUserRepository.java
@@ -4,6 +4,9 @@ import ghdeo.login.entity.CompanyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyUserRepository extends JpaRepository<CompanyUser, Long> {
+    Optional<CompanyUser> findByEmail(String email);
 }

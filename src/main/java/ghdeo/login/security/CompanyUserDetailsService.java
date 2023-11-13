c552d5a5c1c61ebd15b52d5f1b40c5280329de5e
ghdeo <goldencorn7@naver.com>
2023-11-13 11:08:48 +0900

    feat: UserDetailsService 구현체 CompanyUserDetailsService 추가

---
+++ src/main/java/ghdeo/login/security/CompanyUserDetailsService.java
@@ -0,0 +1,23 @@
+package ghdeo.login.security;
+
+import ghdeo.login.entity.CompanyUser;
+import ghdeo.login.repository.CompanyUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyUserDetailsService implements UserDetailsService {
+    private final CompanyUserRepository companyUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        CompanyUser companyUser = companyUserRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("회원 정보 를 찾지 못했습니다."));
+        return CompanyUserDetails.of(companyUser);
+    }
+}

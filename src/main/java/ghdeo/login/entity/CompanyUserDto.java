f40b6b475d73317c7d21e0f736a20c9d4b007cc1
ghdeo <goldencorn7@naver.com>
2023-11-14 18:50:50 +0900

    feat: CompanyUserDto 추가

---
+++ src/main/java/ghdeo/login/entity/CompanyUserDto.java
@@ -0,0 +1,18 @@
+package ghdeo.login.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CompanyUserDto {
+    private Long corp_seq; // 기업 인덱스
+    private String email; // 관리자 이메일
+    private String corp_pw; // 패스워드
+    private String corp_name; // 기업 명
+    private String phone; // 관리자  연락처
+    private String corp_code; // 기업 고유번호
+    private String accessToken;
+}

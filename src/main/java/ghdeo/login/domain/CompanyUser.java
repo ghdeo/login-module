276884204c0d086e379b8277860aa6b43803e30c
ghdeo <goldencorn7@naver.com>
2023-11-19 02:42:47 +0900

    feat: CompanyUser Builder 추가

--- src/main/java/ghdeo/login/domain/CompanyUser.java
+++ src/main/java/ghdeo/login/domain/CompanyUser.java
@@ -1,6 +1,7 @@
 package ghdeo.login.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompanyUser {

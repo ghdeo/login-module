f8bef4bd27f7d047180fb37910b3cc61a4d54223
ghdeo <goldencorn7@naver.com>
2023-11-20 00:34:08 +0900

    feat: SwaggerConfiguration 추가

--- src/main/java/ghdeo/login/config/WebSecurityConfig.java
+++ src/main/java/ghdeo/login/config/WebSecurityConfig.java
@@ -28,7 +28,9 @@ public class WebSecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 // / 경로는 인증 안 해도 된다.
-                .authorizeRequests().antMatchers("/").permitAll()
+                .authorizeRequests().antMatchers("/", "/auth/**",
+                        "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**",
+                        "/api/v3/**", "/swagger-resources/**", "/webjars/**").permitAll()
                 // /  이외의 모든 경로는 인증해야 된다.
                 .anyRequest().authenticated()
                 .and()

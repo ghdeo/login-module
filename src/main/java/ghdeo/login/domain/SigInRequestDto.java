2395141dadefa3ff0d5d1c793b3e55d26f85ec5d
ghdeo <goldencorn7@naver.com>
2023-11-20 00:46:48 +0900

    feat: SignIn Dto 정의

---
+++ src/main/java/ghdeo/login/domain/SigInRequestDto.java
@@ -0,0 +1,15 @@
+package ghdeo.login.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class SigInRequestDto {
+    private String email; // 관리자 이메일
+    private String corp_pw; // 패스워드
+}
---
+++ src/main/java/ghdeo/login/domain/SignInResponseDto.java
@@ -0,0 +1,15 @@
+package ghdeo.login.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class SignInResponseDto {
+    private String email; // 관리자 이메일
+    private String access_token; // access token
+}

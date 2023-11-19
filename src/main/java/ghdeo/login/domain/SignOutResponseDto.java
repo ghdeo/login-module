1d9bd12ee2b5578e4fbdf707acb0deeb55a5a0c5
ghdeo <goldencorn7@naver.com>
2023-11-20 00:47:01 +0900

    feat: SignOut Dto 정의

---
+++ src/main/java/ghdeo/login/domain/SignOutResponseDto.java
@@ -0,0 +1,14 @@
+package ghdeo.login.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class SignOutResponseDto {
+    private String email; // 관리자 이메일
+}
